6ffa26fa412db9f25bb58be7c7a1ed554b54c287
gautav2003 <270303649@yoobeestudent.ac.nz>
2026-01-15 20:18:22 +1300

    Add files via upload

--- New folder/app.py
+++ New folder/app.py
@@ -7,5 +7,13 @@ def index():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-if
+if request.method == 'POST':
+    data = request.get_json()
+    username = data.get('username','').strip()
+    email = data.get('email', '').strip()
+    password = data.get('password','')
+    confirm_password = data.get('confirm_password','')
+    master_key = data.get('master_key','')
+
+    
 
