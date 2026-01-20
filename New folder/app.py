8c463de851d4a6fdb7075dcac5a76e96955bd7af
gautav2003 <270303649@yoobeestudent.ac.nz>
2026-01-20 18:32:44 +1300

    Add files via upload

--- New folder/app.py
+++ New folder/app.py
@@ -4,7 +4,16 @@ EMAIL_ADDRESS = "your_email@gmail.com"
 EMAIL_PASSWORD = "your_app_password"
 
 def send_email(recipient_email, subject, body, is_html=False):
-    
+    try:
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = EMAIL_ADDRESS
+        msg['To'] = recipient_email
+
+        if is_html:
+            msg.attach(MIMEText(body, 'html'))
+             
+
 # =================== AUTH ROUTES ====================
 @app.route('/')
 def index():
