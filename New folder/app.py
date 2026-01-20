518fd2adfea403bb78313cbb2ae61dcf9cb5e0ff
gautav2003 <270303649@yoobeestudent.ac.nz>
2026-01-20 20:24:53 +1300

    Add files via upload

--- New folder/app.py
+++ New folder/app.py
@@ -6,6 +6,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# =================== DATABASE MODELS ================
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    
 # =================== EMAIL CONFIGURATION ============
 
 EMAIL_ADDRESS = "your_email@gmail.com"
